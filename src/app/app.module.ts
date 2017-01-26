e10f5464dab355a0caad7f42190f62ae5abb90b1
Oscar Alberto Paredes Garduno <oscar.paredes@infotec.mx>
2017-01-26 12:53:36 -0600

    Componente base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BaseComponent } from './components/base/base.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BaseComponent
   ],
   imports: [
     BrowserModule,
