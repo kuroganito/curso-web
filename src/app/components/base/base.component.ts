2b67465105e96da20830d4553fd38e4ff7b2d4be
Oscar Alberto Paredes Garduno <oscar.paredes@infotec.mx>
2017-01-26 13:33:28 -0600

    Componente base contenido

--- src/app/components/base/base.component.ts
+++ src/app/components/base/base.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-base',
-  templateUrl: './base.component.html',
-  styleUrls: ['./base.component.css']
+    selector: 'app-base',
+    templateUrl: './base.component.html',
+    styleUrls: ['./base.component.css']
 })
 export class BaseComponent implements OnInit {
 
-  constructor() { }
+    titulo:string;
+    peliculas:Array<any>;
+    
+    constructor() {
+        this.titulo = "InfoCine";
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
